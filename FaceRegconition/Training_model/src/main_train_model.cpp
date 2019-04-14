62fc54b523b9b60af9abd4843beaa6bb103ee0aa
jake <nbkhuong97@gmail.com>
2019-04-14 15:53:59 +0700

    adding data_preparation

--- FaceRegconition/Training_model/src/main_train_model.cpp
+++ FaceRegconition/Training_model/src/main_train_model.cpp
@@ -2,7 +2,6 @@
 #include "../header/func_face_regcontion.hpp"
 int main(int argc, const char *argv[])
 {
-
 	//initial variables
 	string path_training_set = "/home/jake/Desktop/Projects/Cpp/FaceRegconition/Training_model/res/training_set.csv";
 	string output_folder = "output";
